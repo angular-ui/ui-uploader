0ebba30dd730866f579c86544232d4ade84198ce
Todd Branchflower <todd.branchflower@gmail.com>
2015-10-30 20:51:30 -0600

    add some initial tests

--- test/uploaderSpec.js
+++ test/uploaderSpec.js
@@ -1,17 +1,56 @@
 describe('uiUploader', function() {
     'use strict';
 
-    var $scope, $compile, elm;
+    var uiUploader;
 
     beforeEach(module('ui.uploader'));
-    beforeEach(inject(function(_$rootScope_, _$compile_) {
-        $compile = _$compile_;
-        $scope = _$rootScope_.$new();
+    beforeEach(inject(function(_uiUploader_) {
+        uiUploader = _uiUploader_;
     }));
 
-    // TODO add real tests
-    it('dummy test', function() {
-        expect(true).toBeTruthy();
+    describe('#addFiles', function() {
+        it('adds files to the file array', function() {
+            var file = new File([''], 'testFile');
+            uiUploader.addFiles([file]);
+
+            expect(uiUploader.files).toEqual([file]);
+        });
     });
 
+    describe('#removeFile', function() {
+        it('removes the specified file from the file array', function() {
+            var fileToDelete = new File([''], 'fileToDelete');
+            var fileToKeep = new File([''], 'fileToKeep');
+            uiUploader.addFiles([fileToKeep, fileToDelete]);
+
+            uiUploader.removeFile(fileToDelete);
+
+            expect(uiUploader.files).toEqual([fileToKeep]);
+        });
+    });
+
+    describe('#removeAll', function() {
+        it('removes all files from the file array', function() {
+            var files = [1,2,3,4,5].map(function() {
+                return new File([''], 'testFile');
+            });
+            uiUploader.addFiles(files);
+
+            uiUploader.removeAll();
+
+            expect(uiUploader.files).toEqual([]);
+        });
+    });
+
+    describe('#files / #getFiles', function() {
+        it('returns the added files', function() {
+            var files = [1,2,3,4,5].map(function() {
+                return new File([''], 'testFile');
+            });
+            uiUploader.addFiles(files);
+
+            expect(uiUploader.getFiles()).toEqual(files);
+            expect(uiUploader.files).toEqual(files);
+        });
+    });
 });
